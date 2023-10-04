000580a3193d7e5346c1aac70506b07d7b4a9ce2
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-05 00:43:03 +0300

    fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if length > 0:
+        fib_list.append(0)
+    if length >= 2:
+        fib_list.append(1)
+        for i in range(2, length):
+            fib_list.append(fib_list[-1] + fib_list[-2])
+    print(fib_list)
+
